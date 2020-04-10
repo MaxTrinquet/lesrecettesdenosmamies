863a8f5de15d41c422dafd159cd03a3cbff5f172
Maxime Trinquet <maxime.trinquet@gmail.com>
2020-04-10 17:10:14 +0800

    css and routes (new famille)

---
+++ app/controllers/familles_controller.rb
@@ -0,0 +1,25 @@
+class FamillesController < ApplicationController
+
+  def show
+  end
+
+  def index
+    @familles = Famille.all
+  end
+
+  def new
+    @famille = Famille.new
+  end
+
+  def create
+    @famille = Famille.new(params_famille)
+    @famille.save
+  end
+
+private
+
+  def params_famille
+    params.require(:famille).permit(:name)
+  end
+
+end
--- app/controllers/recettes_controller.rb
+++ app/controllers/recettes_controller.rb
@@ -3,7 +3,6 @@ class RecettesController < ApplicationController
   def index
     @recettes = Recette.all
   end
-
   def show
     @recette = Recette.find(params[:id])
   end
