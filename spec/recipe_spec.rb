a56eea2195d0ad607d48deca59a2d4b1069310bb
Maxime Trinquet <maxime.trinquet@gmail.com>
2020-07-02 15:45:55 +0200

    first Rspec

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
+describe 'navigate' do
+  before do
+    @recipe = Recipe.create(name: "My Post")
+  end
 
+  it 'loads the show page' do
+    visit "/recipe/#{@recipe.id}"
+    expect(page.status_code).to eq(200)
+  end
 
-
+  it 'shows the title on the show page in an h1 tag' do
+    visit "/recipe/#{@recipe.id}"
+    expect(page).to have_css("h1", text: "124")
+  end
+end
