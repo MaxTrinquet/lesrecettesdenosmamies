27228021c8260bc7f01ee2a84096b2007e9bea14
Maxime Trinquet <maxime.trinquet@gmail.com>
2020-07-07 12:37:28 +0200

    search fonction for plat - dessert

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,18 @@ class UsersController < ApplicationController
     @user = current_user
     @first_five_current_user_recipes = current_user.recipes.first(5)
     @family_recipe = current_user.family.recipes
+    @results = []
+
+    if params[:query].present?
+      sql_query = " \
+        recipes.category ILIKE :query \
+      "
+      @queried_category = params[:query]
+      @result_arr = Recipe.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @result_arr = current_user.family.recipes
+    end
+
+
   end
 end
